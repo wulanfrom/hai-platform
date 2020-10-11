052f3648f80753428523422de58d7f4e3e9cd8d7
Umar Taufiqulhakim <umartaufiqul@gmail.com>
2020-10-11 08:22:35 +0700

    Fixed Navbar

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
@@ -15,7 +15,7 @@ import {AiOutlineQuestionCircle} from 'react-icons/ai'
 
 const Navigation = () => {
     const sectionList = ["1. Exploring datasets and model performance", "2. Implementing LIME", "3. Create Your Own Visualization"]
-    const section = useSelector(state => state.selection)
+    const section = useSelector(state => state.section)
     const dispatch = useDispatch()
 
     const usePrev = () => {
@@ -30,6 +30,21 @@ const Navigation = () => {
         return section > 0 && section < 4
     }
 
+    useEffect(() => {
+        if (window.location.href.includes("exploration")) {
+            dispatch(setSection(1))
+        }
+        else if (window.location.href.includes("implementation")) {
+            dispatch(setSection(2))
+        }
+        else if (window.location.href.includes("visualization")) {
+            dispatch(setSection(3))
+        }
+        else if (window.location.href.includes("fin")) {
+            dispatch(setSection(4))
+        }
+    })
+
     function goToPage(page_index){
         switch(page_index) {
             case 1:
@@ -38,12 +53,12 @@ const Navigation = () => {
                 break;
                 // window.location.reload()
             case 2:
-                window.location.href = "/hai-platform/#/implemenation"
+                window.location.href = "/hai-platform/#/implementation"
                 dispatch(setSection(2))
                 break;
                 // window.location.reload()
             case 3:
-                window.location.href = "/hai-platform/#/exploration"
+                window.location.href = "/hai-platform/#/visualization"
                 dispatch(setSection(3))
                 // alert("This page is not available. For now")
                 break;
@@ -52,16 +67,22 @@ const Navigation = () => {
         }
     }
    
+    console.log(section)
     return (
         <div>
             <Navbar className='justify-content-center' id='navbar'>
                 <Navbar.Brand id="navbar-brand">Assignment 2. Explainability and Visualization</Navbar.Brand>
-                <Row className="mx-auto">
+                <NavDropdown title={sectionList[section-1]} className='text-left' id='navbar-dropdown'>
+                    <NavDropdown.Item href="/hai-platform/#/exploration"> {sectionList[0]}</NavDropdown.Item>
+                    <NavDropdown.Item href="/hai-platform/#/implementation"> {sectionList[1]} </NavDropdown.Item>
+                    <NavDropdown.Item href="/hai-platform/#/visualization"> {sectionList[2]} </NavDropdown.Item>
+                </NavDropdown>
+                {/* <Row className="mx-auto">
                     <Col><Link to="/exploration">Exploration</Link></Col>
                     <Col><Link to="/implementation">Implementation</Link></Col>
                     <Col><Link to="/visualization">Visualization</Link></Col>
                     <Col><Link to="/finish">Finish</Link></Col>
-                </Row>
+                </Row> */}
                 <div className='tutorial-button'>
                     <OverlayTrigger
                     key={'left'}
